95aa91784bdabe905f36534c8c5a29aec81ca40b
BigPattyOG <86346146+BigPattyOG@users.noreply.github.com>
2025-12-18 02:31:38 +0000

    updating imports and exported libraries

--- src/bswrapper/__init__.py
+++ src/bswrapper/__init__.py
@@ -1,1 +1,15 @@
-from .bswrapper import APIError, Player, Club, ClubSummary, BSWrapper, rateLimited, Unauthorized, playerNotFound
+from __future__ import annotations
+
+from .bswrapper import BSClient, Player, Club, ClubSummary
+from .exceptions import APIError, Unauthorized, RateLimited, NotFound
+
+__all__ = [
+    "BSClient",
+    "Player",
+    "Club",
+    "ClubSummary",
+    "APIError",
+    "NotFound",
+    "Unauthorized",
+    "RateLimited",
+]
